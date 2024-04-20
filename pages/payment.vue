115199ab39ab91c6cfc10fee5632d94a93f8e4d1
Sajjad Mirshabi <m.sajjad1378@gmail.com>
2024-04-20 22:48:18 +0330

    Paymeny 0.0.2

--- pages/payment.vue
+++ pages/payment.vue
@@ -3,29 +3,31 @@
 <button @click="Payment" class="m-auto border rounded-xl p-8">
   Pay
 </button>
+
+  <form action="" enctype="application/x-www-form-urlencoded"></form>
+
 </template>
 
 <script setup lang="ts">
+import {FetchApi} from '~/utilities/CustomApiFetch';
 
-import {$fetch} from "ofetch";
+const Payment = async ()=>{
+  const result = await FetchApi<string>('/payment/payRequest',{
+    method:'POST'
+  });
+  const response:string[] = result.data?.split(',') ?? [];
 
-const Payment = ()=>{
-  $fetch('https://bpm.shaparak.ir/pgwchannel/enstartpay.mellat',{
-    method:'POST',
-    body:{
-      terminalId:7582383,
-      userName:'7582383',
-      userPassword:'86061644',
-      orderId:'1',
-      amount:10000,
-      localDate:'20240420',
-      localTime:'084530',
-      additionalData:'',
-      callBackUrl:'https://gpsaffron.com/verify',
-      payerId:'1',
-      mobileNo:'09154222478'
-    }
-  })
+  const form = document.createElement('form');
+  form.method = 'POST';
+  form.action = 'https://bpm.shaparak.ir/pgwchannel/startpay.mellat';
+  form.enctype = 'application/x-www-form-urlencoded';
+  const refId = document.createElement('input');
+  refId.name = 'RefId';
+  refId.id = 'RefId';
+  refId.value = response[1];
+  form.appendChild(refId);
+  document.body.appendChild(form)
+  form.submit();
 }
 
 </script>
