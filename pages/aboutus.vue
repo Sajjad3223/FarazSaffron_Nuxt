405ffed006370041c883be4cd159f127c3196252
Sajjad Mirshabi <m.sajjad1378@gmail.com>
2024-04-20 13:32:08 +0330

    About Us Page

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -1,11 +1,117 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
+  <div class="flex flex-col items-center mt-12">
+    <h2 class="text-5xl font-semibold">آشنایی مختصر با <span class="text-brandOrange">جی پی</span></h2>
+    <div class="mt-12 p-8 bg-[#F8F8F8] rounded-2xl">
+      <div class="flex mt-12 items-center justify-between">
+        <strong class="text-2xl opacity-70">به جی پی خوش آمدید</strong>
+        <NuxtLink to="/" class="text-brandOrange font-semibold">جهت رفتن به صفحه اول جی پی کلیک کنید</NuxtLink>
+      </div>
+      <div class="bg-white mt-10 p-8 rounded-xl">
+        <p class="text-[#8D8D8D]">
+          ورود شما را به جی پی، فروشگاه آنلاین خرید زعفران خوش‌آمد می‌گوییم. <br>
+          خوشحالیم که هم اکنون در جی پی، دارنده نماد اعتماد الکترونیکی و ارائه دهنده‌ی <span class="text-brandOrange font-bold">ضمانت هفت روزه</span> حضور دارید <br>
+          به دنیای خاص و شگفت‌انگیز زعفران وارد شوید، از زمین‌های حاصلخیز گرفته تا سفره شما، جایی که خلوص با اصالت برابر می‌شود به‌وسیله زعفران باکیفیت، نماد تجمل و طعم را تضمین می‌کنیم.
+          <br>
+          تمام سعی جی پی بر این است که سفارش‌های شما در خراسان رضوی و خارج از آن، بین 24 تا حداکثر 48 ساعت کاری ساعت به دستتان برسد و همیشه راضی از خرید باشید.
+          <br>
+        </p>
+        <ul class="mt-8 grid grid-cols-3 gap-12">
+          <li class="flex flex-col items-start space-y-4">
+            <strong class="text-2xl before:w-4 before:h-4 before:bg-brandOrange before:rounded-full relative before:absolute pr-8 before:right-0 flex items-center text-[#7E7E7E]">
+              اهداف جی پی
+            </strong>
+            <p class="text-[#8D8D8D] text-justify px-8">
+              اعتقاد و اعتمادی که شما  در هر خرید به ما دارید را ارج می‌نهیم , به همین دلیل است که ما فقط فروشنده نیستیم بلکه نگهبان یک میراث غنی هستیم , وعده ما به شما فقط ارائه زعفران درجه‌یک نیست , بلکه طعم و عطر و رنگی است که شمارا به همان مزارعی می‌رساند که سفر ما ازآنجا آغاز می‌شود . 
+              <br>
+              با حرکت به‌سوی آینده و بهره‌گیری از استانداردهای جهانی و طلایی و استفاده از نوآوری‌های و شیوه‌های پایدار در تجارت و تعهد و تضمین بدون قید و شرط به مشتریان در جهت شناساندن و عرضه زعفران ناب در مسیر چشم‌انداز روان هستیم.
+            </p>
+          </li>
+          <li class="flex flex-col items-start space-y-4">
+            <strong class="text-2xl before:w-4 before:h-4 before:bg-brandOrange before:rounded-full relative before:absolute pr-8 before:right-0 flex items-center text-[#7E7E7E]">
+              دلایل اعتماد
+            </strong>
+            <p class="text-[#8D8D8D] text-justify px-8">
+              بدون هیچ‌شکی، اعتماد لازمه پیشرفت و تداوم هر کسب و کاری است. جی پی با کسب مجوزهای معتبری مثل نماد اعتماد الکترونیکی با نشان ۵ ستاره، عضویت در سازمان صنفی کشور، عضویت در اتحادیه کشوری و کسب‌وکارهای مجازی، عضویت در انجمن صنفی کارفرمایی فروشگاه‌های اینترنتی توانسته است تا تاییدیه‌های لازم برای جلب رضایت کاربرانش را فراهم کند. اطلاعات مربوط به این مجوزها را می‌توانید از لینک <NuxtLink to="/" class="text-brandOrange">مجوز های جی پی</NuxtLink> مشاهده نمایید. در ادامه لازم است به این مورد هم اشاره کنیم که جی پی برگزیده آرای مردمی دوازدهمین جشنوار زعفران  ایران شده است
+            </p>
+          </li>
+          <li class="flex flex-col items-start space-y-4">
+            <strong class="text-2xl before:w-4 before:h-4 before:bg-brandOrange before:rounded-full relative before:absolute pr-8 before:right-0 flex items-center text-[#7E7E7E]">
+              ضمانت <span class="text-brandOrange mx-1">هفت روزه</span> کالا
+            </strong>
+            <p class="text-[#8D8D8D] text-justify px-8">
+              لطفاً کالا را برای بازگرداندن و ارسال آن به خدمات پس از فروش جی پی از طریق پست، تنها به صندوق پستی 1234-5678 ارسال کنید.(شما می‌توانید از طریق بخش سفارش‌های من اقدام به بازگردانی کالا کنید)
+              توجه داشته باشید شما فقط می توانید برای کالا های پایین تر از 5 گرم بازگردانی را انجام دهید.
+            </p>
+          </li>
+        </ul>
+      </div>
 
+      <div class="mt-16 flex flex-col items-center">
+        <span>
+          جهت مشاهده‌ی فعالیت‌های جی پی و آشنایی بیشتر با ما ، به حساب‌ کاربری جی پی در اینستاگرام مراجعه فرمایید.
+        </span>
+        <a href="" class="flex items-center gap-6 text-white bg-brandOrange px-8 py-1 rounded-xl font-bold text-lg mt-6" style="box-shadow: 0 10px 25px 0 #F1482333">
+          <svg class="w-5" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_569_2177)">
+              <path d="M24 4.32187C30.4125 4.32187 31.1719 4.35 33.6938 4.4625C36.0375 4.56562 37.3031 4.95938 38.1469 5.2875C39.2625 5.71875 40.0688 6.24375 40.9031 7.07812C41.7469 7.92188 42.2625 8.71875 42.6938 9.83438C43.0219 10.6781 43.4156 11.9531 43.5188 14.2875C43.6313 16.8187 43.6594 17.5781 43.6594 23.9813C43.6594 30.3938 43.6313 31.1531 43.5188 33.675C43.4156 36.0188 43.0219 37.2844 42.6938 38.1281C42.2625 39.2438 41.7375 40.05 40.9031 40.8844C40.0594 41.7281 39.2625 42.2438 38.1469 42.675C37.3031 43.0031 36.0281 43.3969 33.6938 43.5C31.1625 43.6125 30.4031 43.6406 24 43.6406C17.5875 43.6406 16.8281 43.6125 14.3063 43.5C11.9625 43.3969 10.6969 43.0031 9.85313 42.675C8.7375 42.2438 7.93125 41.7188 7.09688 40.8844C6.25313 40.0406 5.7375 39.2438 5.30625 38.1281C4.97813 37.2844 4.58438 36.0094 4.48125 33.675C4.36875 31.1438 4.34063 30.3844 4.34063 23.9813C4.34063 17.5688 4.36875 16.8094 4.48125 14.2875C4.58438 11.9437 4.97813 10.6781 5.30625 9.83438C5.7375 8.71875 6.2625 7.9125 7.09688 7.07812C7.94063 6.23438 8.7375 5.71875 9.85313 5.2875C10.6969 4.95938 11.9719 4.56562 14.3063 4.4625C16.8281 4.35 17.5875 4.32187 24 4.32187ZM24 0C17.4844 0 16.6688 0.028125 14.1094 0.140625C11.5594 0.253125 9.80625 0.665625 8.2875 1.25625C6.70312 1.875 5.3625 2.69062 4.03125 4.03125C2.69063 5.3625 1.875 6.70313 1.25625 8.27813C0.665625 9.80625 0.253125 11.55 0.140625 14.1C0.028125 16.6687 0 17.4844 0 24C0 30.5156 0.028125 31.3312 0.140625 33.8906C0.253125 36.4406 0.665625 38.1938 1.25625 39.7125C1.875 41.2969 2.69063 42.6375 4.03125 43.9688C5.3625 45.3 6.70313 46.125 8.27813 46.7344C9.80625 47.325 11.55 47.7375 14.1 47.85C16.6594 47.9625 17.475 47.9906 23.9906 47.9906C30.5063 47.9906 31.3219 47.9625 33.8813 47.85C36.4313 47.7375 38.1844 47.325 39.7031 46.7344C41.2781 46.125 42.6188 45.3 43.95 43.9688C45.2812 42.6375 46.1063 41.2969 46.7156 39.7219C47.3063 38.1938 47.7188 36.45 47.8313 33.9C47.9438 31.3406 47.9719 30.525 47.9719 24.0094C47.9719 17.4938 47.9438 16.6781 47.8313 14.1188C47.7188 11.5688 47.3063 9.81563 46.7156 8.29688C46.125 6.70312 45.3094 5.3625 43.9688 4.03125C42.6375 2.7 41.2969 1.875 39.7219 1.26562C38.1938 0.675 36.45 0.2625 33.9 0.15C31.3313 0.028125 30.5156 0 24 0Z" fill="currentColor"/>
+              <path d="M24 11.6719C17.1938 11.6719 11.6719 17.1938 11.6719 24C11.6719 30.8062 17.1938 36.3281 24 36.3281C30.8062 36.3281 36.3281 30.8062 36.3281 24C36.3281 17.1938 30.8062 11.6719 24 11.6719ZM24 31.9969C19.5844 31.9969 16.0031 28.4156 16.0031 24C16.0031 19.5844 19.5844 16.0031 24 16.0031C28.4156 16.0031 31.9969 19.5844 31.9969 24C31.9969 28.4156 28.4156 31.9969 24 31.9969Z" fill="currentColor"/>
+              <path d="M39.6937 11.1843C39.6937 12.778 38.4 14.0624 36.8156 14.0624C35.2219 14.0624 33.9375 12.7687 33.9375 11.1843C33.9375 9.59053 35.2313 8.30615 36.8156 8.30615C38.4 8.30615 39.6937 9.5999 39.6937 11.1843Z" fill="currentColor"/>
+            </g>
+            <defs>
+              <clipPath id="clip0_569_2177">
+                <rect width="48" height="48" fill="currentColor"/>
+              </clipPath>
+            </defs>
+          </svg>
+          <span>اینستاگرام جی پی</span>
+        </a>
+      </div>
+
+      <ul class="grid grid-cols-3 gap-6 mt-12 px-12">
+        <li class="w-full overflow-hidden rounded-xl">
+          <img src="~/assets/images/aboutUs1.png" alt="aboutUs" class="w-full hover:scale-110 transition-transform duration-300">
+        </li>
+        <li class="w-full overflow-hidden rounded-xl">
+          <img src="~/assets/images/aboutUs2.png" alt="aboutUs" class="w-full hover:scale-110 transition-transform duration-300">
+        </li>
+        <li class="w-full overflow-hidden rounded-xl">
+          <img src="~/assets/images/aboutUs3.png" alt="aboutUs" class="w-full hover:scale-110 transition-transform duration-300">
+        </li>
+      </ul>
+
+    </div>
+
+    <!-- Honors -->
+    <section class="w-full mt-16">
+      <div class="flex items-center justify-between">
+        <h3 class="text-2xl font-black pr-4 border-r-4 border-brandOrange">
+          افتخارات
+        </h3>
+      </div>
+      <div class="flex items-center justify-between mt-4">
+        <div class="text-[#8F8F8F] flex flex-col gap-2">
+            <span class="text-sm text-[#8D8D8D] font-light">
+              گواهینامه و افتخارات
+            </span>
+        </div>
+      </div>
+
+      <div class="w-full flex items-end justify-between -translate-y-12">
+        <p class="w-[600px] -translate-y-6 text-[#828282] font-thin">
+          شرکت زعفران جی پی به مدت 7 سال مفتخر به دریافت عنوان “واحد نمونه صنعتی” شده است. شرکت ما با بهره مندی از سیستم مدیریت کیفیت عالی توانسته است گواهینامه های ISO 9001 و نیز 22000 ISO را به دست بیاورد.
+        </p>
+        <div class="flex">
+          <img src="~/assets/images/honor1.png" alt="honor" class="max-w-[170px]">
+          <img src="~/assets/images/honor2.png" alt="honor" class="max-w-[170px]">
+          <img src="~/assets/images/honor1.png" alt="honor" class="max-w-[170px]">
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<script setup lang="ts">
+definePageMeta({
+  layout:'new-layout'
+})
+</script>
---
+++ pages/payment.vue
@@ -0,0 +1,31 @@
+
+<template>
+<button @click="Payment" class="m-auto border rounded-xl p-8">
+  Pay
+</button>
+</template>
+
+<script setup lang="ts">
+
+import {$fetch} from "ofetch";
+
+const Payment = ()=>{
+  $fetch('https://bpm.shaparak.ir/pgwchannel/enstartpay.mellat',{
+    method:'POST',
+    body:{
+      terminalId:7582383,
+      userName:'7582383',
+      userPassword:'86061644',
+      orderId:'1',
+      amount:10000,
+      localDate:'20240420',
+      localTime:'084530',
+      additionalData:'',
+      callBackUrl:'https://gpsaffron.com/verify',
+      payerId:'1',
+      mobileNo:'09154222478'
+    }
+  })
+}
+
+</script>
