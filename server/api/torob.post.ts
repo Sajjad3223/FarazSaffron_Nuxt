5b0ec1625367f87614d71297ee9be0b26a51b831
Sajjad Mirshabi <m.sajjad1378@gmail.com>
2025-11-10 19:49:46 +0330

    Fix TorobPay Issues

--- server/api/torob.post.ts
+++ server/api/torob.post.ts
@@ -1,30 +1,55 @@
 import {TorobPaySettle, TorobPayVerify} from "~/services/torobPay.service";
+import {BASE_URL} from "~/utilities/api.config";
 
 
 export default defineEventHandler( async (event) => {
-    const rawBody = await readRawBody(event);
+    try{
+        const rawBody = await readRawBody(event);
 
-    let body: Record<string, string> = {}
-    if (rawBody) {
-        body = Object.fromEntries(new URLSearchParams(rawBody))
+        let body: Record<string, string> = {}
+        if (rawBody) {
+            body = Object.fromEntries(new URLSearchParams(rawBody))
+        }
+
+        console.log(body);
+
+        if(body.state !== 'OK')
+            return sendRedirect(event, '/payment/failed')
+
+        /*const tokenCookie = getCookie(event,'torobpay-payment');
+        console.log(tokenCookie)
+        if(!tokenCookie)
+            return sendRedirect(event, '/payment/failed')
+
+        const paymentToken = JSON.parse(tokenCookie).paymentToken;
+        console.log(paymentToken);
+
+        const authToken = getCookie(event,'c-access-token');*/
+
+        const verifyResult = await $fetch(`/torobPay/verify?transactionId=${body.transactionId}`,{
+            baseURL:BASE_URL,
+            method:'GET'
+        });
+
+        console.log(verifyResult);
+
+        //const verifyResult = await TorobPayVerify(paymentToken);
+        if(!verifyResult.isSuccess || !verifyResult.data.successful)
+            return sendRedirect(event, '/payment/failed')
+
+        const settleResult = await $fetch(`/torobPay/settle?transactionId=${body.transactionId}`,{
+            baseURL:BASE_URL,
+            method:'GET'
+        });
+
+        //const settleResult = await TorobPaySettle(paymentToken);
+        if(!settleResult.isSuccess || !settleResult.data.successful)
+            return sendRedirect(event, '/payment/failed')
+
+        return sendRedirect(event, `/payment/success?saleRefId=${settleResult.data}&orderId=${body.transactionId}`)
     }
-
-    if(body.state !== 'OK')
+    catch (e) {
+        console.log(e);
         return sendRedirect(event, '/payment/failed')
-
-    const tokenCookie = getCookie(event,'torobpay-payment');
-    if(!tokenCookie)
-        return sendRedirect(event, '/payment/failed')
-
-    const paymentToken = tokenCookie.paymentToken;
-
-    const verifyResult = await TorobPayVerify(paymentToken);
-    if(!verifyResult.isSuccess || !verifyResult.data.successfull)
-        return sendRedirect(event, '/payment/failed')
-
-    const settleResult = await TorobPaySettle(paymentToken);
-    if(!settleResult.isSuccess || !settleResult.data.successfull)
-        return sendRedirect(event, '/payment/failed')
-
-    return sendRedirect(event, '/payment/success')
+    }
 })
